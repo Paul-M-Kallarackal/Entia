d6f4fd4aa671b9819513ba619334d770ab1a6cf5
Paul-M-Kallarackal <paulmkallarackal@gmail.com>
2025-08-30 22:36:38 +0530

    removed createRectangle code

--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -14,18 +14,11 @@ import "./App.css";
 import { AddOnSDKAPI } from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
 
 const App = ({ addOnUISdk, sandboxProxy }: { addOnUISdk: AddOnSDKAPI; sandboxProxy: DocumentSandboxApi }) => {
-    function handleClick() {
-        sandboxProxy.createRectangle();
-    }
-
     return (
         // Please note that the below "<Theme>" component does not react to theme changes in Express.
         // You may use "addOnUISdk.app.ui.theme" to get the current theme and react accordingly.
         <Theme system="express" scale="medium" color="light">
             <div className="container">
-                <Button size="m" onClick={handleClick}>
-                    Create Rectangle
-                </Button>
             </div>
         </Theme>
     );
